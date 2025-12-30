bb6552d29844d79871a63a8ace412ddb60c9d72a
deformed-steve <koltnapier2014@gmail.com>
2025-12-29 20:19:55 -0500

    f

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+  challenge: false,
+  users: {
+    daddy: "6969"
+  }
+};
+
+export default config;
+
+// What are you doing here if you are here u are 🏳️‍🌈
+// jk u are not THAT but still use npm start and then npm start
+// share this w/ ur friends :D
